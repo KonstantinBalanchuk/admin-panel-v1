b44ba5f032476e5319ce0be97ca050bad83210d0
Konstantin Balanchuk <konstantinbalanchuk@gmail.com>
2019-11-09 21:12:54 +0200

    added entry point to webpack and first component with two post requests to telegram api

--- src/js/components/container/Main.js
+++ src/js/components/container/Main.js
@@ -1,21 +1,54 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, {Component} from "react";
 
 class Main extends Component {
-    constructor() {
-        super();
-        this.state = {
-            title: ""
-        };
+
+    async postData(url = '', data = {}) {
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrer: 'no-referrer', // no-referrer, *client
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        console.log(response.json());
+        return await response.json(); // parses JSON response into native JavaScript objects
     }
+
+    sendmessage = async () => {
+        // const apiUrl = 'https://api.telegram.org/bot';
+        // const token = '895555100:AAFNqaj6zHSlJ1QzChvln7j2ki5kxxVrlt8';
+        // const method = 'sendMessage';
+
+        try {
+            const data = await this.postData('https://api.telegram.org/bot895555100:AAFNqaj6zHSlJ1QzChvln7j2ki5kxxVrlt8/sendPoll', {
+                'chat_id': -332588433,
+                // 'text': 'hello world'
+                'question': 'btfht',
+                'options': ['gb', 'Нgbf']
+            });
+            console.log(data); // JSON-string from `response.json()` call
+        } catch (error) {
+            console.error(error);
+        }
+
+        // this.postData('https://api.telegram.org/bot895555100:AAFNqaj6zHSlJ1QzChvln7j2ki5kxxVrlt8/sendMessage', {'chat_id': 461497162, 'text': 'hello world'} )
+        // return this.postData('https://api.telegram.org/bot895555100:AAFNqaj6zHSlJ1QzChvln7j2ki5kxxVrlt8/sendPoll', {'chat_id': 461497162, 'question': 'btfht', 'options': ['gb','Нgbf']} )
+
+    };
+
     render() {
         return (
-            <h1>Hello world</h1>
-    );
+            <div>
+                <h1>Hello world</h1>
+                <button onClick={this.sendmessage}>send message</button>
+            </div>
+        );
     }
 }
 
 export default Main;
-
-const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Main />, wrapper) : false;
